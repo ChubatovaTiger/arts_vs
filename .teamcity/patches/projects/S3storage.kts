a14e003d43358e234de2dbf12b17190247be9d36
admin <admin@TeamCity>
2022-07-09 14:35:14 +0200

    TeamCity change in 'arts / s3storage' project: Artifacts storage settings updated

--- .teamcity/patches/projects/S3storage.kts
+++ .teamcity/patches/projects/S3storage.kts
@@ -28,6 +28,7 @@ changeProject(RelativeId("S3storage")) {
         }
         feature1.apply {
             bucketPrefix = "pref_chubatova2"
+            enablePresignedURLUpload = false
             param("aws.use.default.credential.provider.chain", "")
             param("storage.s3.acl", "")
         }
